1a5fb72f35fcb86477d5a5146d964e66a841dd5e
Brandon Czaja <Brandon.Czaja@gmail.com>
2020-08-26 08:38:18 -0500

    Able to get Card Image Gallery to work. Next step is to make it dynamic

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 /*
 THINGS I NEED TO FIX
-    -IF CARD IS VANILLA THE RULES TEXT DOESN'T CHANGE
-        -IF(RULES === FALSE) {DISPLAY THIS CARD HAS NO SPECIAL RULES}
-    -I NEED TO LIMIT THE DISPLAY WIDTH OF TH DL SO THAT MY IMAGES DON'T FLY TO THE RIGHT SIDE IF THE TEXT IS LONGER THAN AVERAGE
     -NOT REAL CARD OR TYPO?
     -NEED MISSING SET SYMBOLS
     -FIX SET SYMBOL PLACEMENT AND SIZES
+    -FIX WINDOW BACKGROUND FOR DIFFERENT SCREEN SIZES
+        -USE TABLET/PHONE BUTTON IN INSPECT
 
     
 
@@ -13,15 +12,6 @@ THINGS I NEED TO FIX
     STRETCH:
         -ON CLICK #BOTTOM TOP BORDER 5PX SOLID BLACK
 */
-
-
-////// TEST CODE FOR DYNAMICALLY ADDING IMAGES.... THIS WORKS FOR THE SET SYMBOLS BUT NOW I NEED TO MAKE IT FOR THE CARD GALLERY WHEN I CLICK THE SET SYMBOL IMAGE//////
-
-
-//I think the solution to this is going to be loop through an array of images > append each image to the body > give each image an id of i
-
-    //$('#bottom').append($('#1').css('background-color', 'white'));
-/////////////////////////////////////////////
 
 
 
@@ -42,19 +32,27 @@ $('form').on('submit' , (event) => {
         url:`https://api.magicthegathering.io/v1/cards?name=${card}`,
     }).then(
         (data) => {
+            console.log(data)
                 //Card Image
                 $('#card-image').html(`<img src = "${data.cards[0].imageUrl}" 'width = 25%'>`)
-                //Card Name
-                //$('#name').html(data.cards[0].name);
                 //Mana Cost
                 $('#mana').html(data.cards[0].manaCost);
                 //Type
                 $('#type').html(data.cards[0].type);
                 //Rarity
                 $('#rarity').html(data.cards[0].rarity);
-                //Rules Text
-                    //If card is vanilla rules text says loading...
+                //Power & Toughness
+                if(data.cards[0].power === undefined) {
+                    $('#power').text('This card is not a creature');
+                } else {
+                $('#power').html(data.cards[0].power + '/' + data.cards[0].toughness);
+                }
+                //Card Text
+                if(data.cards[0].text === undefined) {
+                    $('#card-text').text('This card does not have any rules text');
+                } else {
                 $('#card-text').html(data.cards[0].text);
+                }
             })    
             }) , () => {
             $('form').append($('<div>').text('ERROR: Please try another card name.'));
@@ -68,11 +66,11 @@ $('form').on('submit' , (event) => {
 
 //////////////////////////////////////////////////////////////////////
 
-////////////////////////////Carousel//////////////////////////////////
+///////////////////////    Carousel    //////////////////////////////////
 let currentImageIndex = 0;
 let highestIndex = $('.carousel-images').children().length-1
 
-
+//Next Button
 $('.next').on('click' , () => {
     console.log('next was clicked');
     $('.carousel-images').children().eq(currentImageIndex).css('display' , 'none');
@@ -86,6 +84,7 @@ $('.next').on('click' , () => {
     $('.carousel-images').children().eq(currentImageIndex).css('display' , 'block');
 })
 
+//Previous Buttons
 $('.previous').on('click' , () => {
     $('.carousel-images').children().eq(currentImageIndex).css('display', 'none');
 
@@ -110,83 +109,52 @@ console.log(highestIndex)
 
 
 
+
+
 //////////// CAROUSEL CLICK = DISPLAY SET ///////////
-let $setSymbol = $('#results');
-//url:`https://api.scryfall.com/sets/SETNAME?as=grid&order=set`
-//So what the url says is search this site by sets => set name > display it as a grid => in the order that the cards are listed in the set
-
-
-//Object path to Card Names `https://api.scryfall.com/cards/search?order=set&q=e%3Awar&unique=prints`
-    //object.data[i].name
-
-
-
-// const getSetSymbol = () => {
-
-//   $.ajax({
-//         url: `https://api.scryfall.com/cards/search?order=set&q=e%3Awar&unique=prints`
-//     }).then(
-//         (data) => {
-//             console.log('you clicked the set symbol');
-//            $($setSymbol).append(data);
-//             console.log(data);
-//         }
-//     )
-// }
-
-// $('img').on('click' , getSetSymbol);
-
-
-//BREAKTHROUGH FOR CAROUSEL IMAGES
-    //SCRYFALL HAS A SEARCH_URI PROPERTY WITH AN URL THAT GIVES THE LIST OF EVERY CARD IN THE SET
+                //use sets/name for h1
 
 
 
 
 
+const $makeGallery = () => {
+    for(let i = 0; i < imagesArray.length; i++) {
+        $cardImageGalery = $('<img>').attr({
+            src: imagesArray[i],
+            title: titlesArray[i]
+    });
+    }
+}
+
+const $img = $('<img>').attr('title' , 'aer')
+$('.carousel-images').on('click' , (event) =>{
+    $.ajax({
+        //url: `https://api.scryfall.com/cards/search?order=set&q=e%3A${$img}&unique=prints`
+        url: `https://api.scryfall.com/cards/search?order=set&q=e%3Aaer&unique=prints`
+    }).then (
+        (data) => {
+            
+            //console.log(data.data)
+            console.log(data);
+           for(let i = 0; i < data.data.length; i++) {
+            $('<img>').attr('src', data.data[i].image_uris.normal).appendTo($('#bottom'));
+           }
+        }
+    )
+})
 
 
 
+//make an array of objects that has all the information I need. The loop through that and create a variable for the current index. Then `` with the thing i need at current index
 
 
+const $cardImageGalery = [
+    {
+        set_name: 'Aether Revolt',
+        imgUrl: 'Aether_Revolt.png',
+    }
+
+]
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Use a different API for the carousel, my original one doesn't have the set info
-
-//Carousel API
-    //I may need this api https://mtgjson.com/data-models/set/
-    //Or this one https://api.scryfall.com
-
-// const setSymbol = $('.carousel-images')
-
-// $.ajax({
-//     url: `https://api.magicthegathering.io/v1/sets?name=${setSymbol}`,
-// }).then(
-//     (data) => {
-//         $('.carousel-images').html(data.sets.name);
-//     }
-// )
-
-
-
-
-
-//Making the set images a button
-    //Put an event handler on the image and do event.target
