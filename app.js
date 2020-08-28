9d137bbbdcf09ea68b9bc981cab3b2e3421ab7a1
Brandon Czaja <Brandon.Czaja@gmail.com>
2020-08-28 13:27:50 -0500

    Cleaned up my code, added styling. Next is to make more responsive

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-//////////////////INDIVIDUAL CARDS//////////////////////////////////
-//GOOD NEWS: RECEIVED A 503 ERROR: THEY ARE TEMPORARILY OFFLINE FOR MAINTENANCE 
+//////////////////    INDIVIDUAL CARDS    //////////////////////////////////
 $('form').on('submit' , (event) => {
     event.preventDefault();
-
     const card = $('input').val();
-
     $.ajax({
-        url:`https://api.magicthegathering.io/v1/cards?name=${card}`,
+        url:`https://api.magicthegathering.io/v1/cards?name=${card}`
     }).then(
         (data) => {
-            console.log(data)
+            console.log(data);
+            // if(data.cards[0].text === undefined) {
+            //     $('#card-text').text('This card does not have any rules text');
+            // } else {
+            // $('#card-text').html(data.cards[0].text);
+            // }IF DATA.CARDS[0].NAME != INPUT => ERROR
+            
                 //Card Image
-                $('#card-image').html(`<img src = "${data.cards[0].imageUrl}" 'width = 25%'>`)
+                $('#card-image').html(`<img src = "${data.cards[0].imageUrl}" 'width = 25%'>`);
                 //Mana Cost
                 $('#mana').html(data.cards[0].manaCost);
                 //Type
@@ -32,129 +35,66 @@ $('form').on('submit' , (event) => {
                 }
             })    
             }) , () => {
-            $('form').append($('<div>').text('ERROR: Please try another card name.'));
-            console.log('ERROR: Please try another card name.')
+                //THIS DOESN'T WORK
+            $('.split left').append($('<div>').text('ERROR: Please try another card name.'));
+            console.log('ERROR: Please try another card name.');
         }
 
 
 
-
-
-
-////////////////////////////////////////////////////////////////////
-
 ///////////////////////    Carousel    //////////////////////////////////
-/*
--POTENTIAL NEW API. SAME AS THE CARD SEARCH API BUT FOR SETS NOT CARDS
-https://api.magicthegathering.io/v1/sets?SETNAME= THEN HOWEVER I CALL IT
+$.ajax({
+//URL FOR SET NAMES AND ICONS
+url: `https://api.scryfall.com/sets/` 
+}).then(
+    (data) =>{
+        //console.log(data)
+        let highestIndex = data.data.length-1;
+        let currentImageIndex = 0;
+        for(let i = 0; i <= highestIndex; i++){
+            $('<img class = SetSymbols>').attr({src: data.data[i].icon_svg_uri , class: `${i}`}).appendTo($('.carousel-images'));
+            $('.set-search').text(data.data[currentImageIndex].name);      
+        }          
+        
+        //Next Button
+        $('.next').on('click' , () => {
+            $('.carousel-images').children().eq(currentImageIndex).css('display' , 'none');
 
--I WONDER IF I CAN CHANGE THE CAROUSEL TO BE ALL IMAGE VARIATIONS OF THE CARD.
-*/
-
-        // $.ajax({
-        //     url: 'https://api.magicthegathering.io/v1/sets?'
-        // }).then(
-        //     (data) => {
-        //         console.log(data);
-        //         let highestIndex = data.length-1;
-        //         let currentImageIndex = 0;
-
-        //         for(let i = 0; i < highestIndex; i++) {
-        //             $('<img class = SetSymbols>').attr({src: data[i].imageUrl}).appendTo($('.carousel-images'));
-        //         }
-                
-        //         //Next Button
-        //         $('.next').on('click' , () => {
-        //             console.log('next was clicked');
-        //             $('.carousel-images').children().eq(currentImageIndex).css('display' , 'none');
-                
-        //             if(currentImageIndex < highestIndex) {
-        //                 currentImageIndex++;
-        //             } else {
-        //                 currentImageIndex = 0;
-        //             }
-                
-        //             $('.carousel-images').children().eq(currentImageIndex).css('display' , 'block');
-        //         })
-
-        //         //Previous Buttons
-        //         $('.previous').on('click' , () => {
-        //             $('.carousel-images').children().eq(currentImageIndex).css('display', 'none');
-                
-        //             if(currentImageIndex > 0) {
-        //                 currentImageIndex--;
-        //             } else {
-        //                 currentImageIndex = highestIndex;
-        //             }
-                
-        //             $('.carousel-images').children().eq(currentImageIndex).css('display', 'block');
-        //         })
-        //     }
-        // )
+            if(currentImageIndex < highestIndex) {
+                currentImageIndex++;
+            } else {
+                currentImageIndex = 0;
+            }
+        
+            $('.carousel-images').children().eq(currentImageIndex).css('display' , 'block');
+            $('.set-search').text(data.data[currentImageIndex].name);
+        })
+        
+        //Previous Buttons
+        $('.previous').on('click' , () => {
+            $('.carousel-images').children().eq(currentImageIndex).css('display', 'none');
+        
+            if(currentImageIndex > 0) {
+                currentImageIndex--;
+            } else {
+                currentImageIndex = highestIndex;
+            }
+        
+            $('.carousel-images').children().eq(currentImageIndex).css('display', 'block');
+            $('.set-search').text(data.data[currentImageIndex].name);
+        })
+    }
+)
 
 
 
- 
-          
-          
-
-          
-////////////////////////////////////////////////////////////////////
-//SET ICONS
-                                    //I WANT TO LIMIT MY SEARCH TO JUST CORE AND EXPANSION SETS
-
-  $.ajax({
-    //URL FOR SET NAMES AND ICONS
-    url: `https://api.scryfall.com/sets/` 
-  }).then(
-      (data) =>{
-            //console.log(data)
-            let highestIndex = data.data.length-1;
-            let currentImageIndex = 0;
-
-            for(let i = 0; i <= highestIndex; i++){
-                $('<img class = SetSymbols>').attr({src: data.data[i].icon_svg_uri , class: `${i}`}).appendTo($('.carousel-images'));
-                
-                $('.set-search').text(data.data[currentImageIndex].name);      
-            }          
-            
-            
-
-
-
-          //Next Button
-          $('.next').on('click' , () => {
-              $('.carousel-images').children().eq(currentImageIndex).css('display' , 'none');
-
-              if(currentImageIndex < highestIndex) {
-                  currentImageIndex++;
-              } else {
-                  currentImageIndex = 0;
-              }
-          
-              $('.carousel-images').children().eq(currentImageIndex).css('display' , 'block');
-              $('.set-search').text(data.data[currentImageIndex].name);
-          })
-          
-          //Previous Buttons
-          $('.previous').on('click' , () => {
-              $('.carousel-images').children().eq(currentImageIndex).css('display', 'none');
-          
-              if(currentImageIndex > 0) {
-                  currentImageIndex--;
-              } else {
-                  currentImageIndex = highestIndex;
-              }
-          
-              $('.carousel-images').children().eq(currentImageIndex).css('display', 'block');
-              $('.set-search').text(data.data[currentImageIndex].name);
-          })
-
-          
-
-      }
-  )
-
+/*                               THINGS TO ADD IN THE FUTURE
+REJECTION NOTICE IF CARD DOES NOT EXIST
+CARD IMAGE GALLERY
+DECK BUILDING FEATURE
+DRAFTING FEATURE
+LIMIT MY CAROUSEL TO CORE AND EXPANSION SETS INSTEAD OF EVERY PRINTING
+*/ 
 
 
 //CARD GALLERY
@@ -183,86 +123,3 @@ https://api.magicthegathering.io/v1/sets?SETNAME= THEN HOWEVER I CALL IT
 //       }
 //   )
 // })
-
-
-//////////////////////////////////////////////////////////////////////
-
-
-//APIs THAT WORK FOR ME
-/*
-    -THIS GETS ME THE CARD IMAGE GALLERY SPECIFICALLY FOR AER: WORKS!
-    url: `https://api.scryfall.com/cards/search?order=set&q=e%3Aaer&unique=prints`
-
-
-    -THIS GETS ME THE SET OBJECT WITH POSSIBLY THE SET ICON... (ICON_SVG_URI)
-    url: `https://api.scryfall.com/sets/aer`
-
-
-    -URL SYNTAX I THINK I NEED TO USE
-    url: `https://api.scryfall.com/cards/search?order=set&q=e%3A${$img}&unique=prints`
-
-
-/             //SET ICONS
-//             $('<img class = SetSymbols>').attr('src', data.data[i].icon_svg_uri).appendTo($('#bottom'));
-//             //SET NAMES
-//             //$('<p>').text(data.data[i].name).appendTo('#bottom');
-//             //CARD GALLERY
-//             //$('<img>').attr('src' , data.data[i].image_uris.normal).appendTo('#bottom');
-//         //NOT REALLY SURE WHAT THIS IS RETURNING
-//         url: "https://c2.scryfall.com/file/scryfall-symbols/sets/aer.svg?1598241600",
-//         //URL FOR CARD GALLERY
-//         url: 'https://api.scryfall.com/cards/search?order=set&q=e%3Aaer&unique=prints',
-//          //URL FOR SET NAMES AND ICONS
-//         url: `https://api.scryfall.com/sets`
-*/
-
-
-
-
-// let myArray = []
-
-
-
-// let test = () => {
-
-//     $.ajax({
-//         url: `https://api.scryfall.com/sets`
-//     }).then (
-//         (data) => {
-//             myArray = data.data;
-//             console.log(myArray);
-//             for(let i = 0; i < data.data.length; i++) {
-//                 myArray.push({
-//                     Name: data.data[i].name,
-//                     Icon: data.data[i].icon_svg_uri,
-//                     Gallery: data.data[i].search_uri
-//                 })
-//                 //$('<p>').text(data.data[i].name).prependTo('#bottom');
-//             //}
-//                $('.carousel-images').on('click' , (event) => {
-//                 myArray.forEach(element => {
-//                     $('<img>').attr('src' , myArray.Gallery).appendTo($('#bottom'));
-//                 });
-        
-//             })
-
-
-            
-//         }
-
-//     })
-// }
-// test();
-
-
-// $('.carousel-images').on('click' , () => {
-//     $.ajax({
-//           //URL FOR CARD GALLERY
-//           url: 'https://api.scryfall.com/cards/search?order=set&q=e%3Aaer&unique=prints',   
-//     }).then(
-//         (data) => {
-//             for(let i = 0; i < data.data.length; i++) {
-//                $('<img>').attr('src' , myArray[i].Gallery).appendTo($('#bottom'));
-//             }
-//         })}
-// )
