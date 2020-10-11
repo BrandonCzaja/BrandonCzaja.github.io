18c27bb20d1dedc09e7249ba6530529efce703ee
Brandon Czaja <Brandon.Czaja@gmail.com>
2020-10-11 12:53:40 -0500

    Able to print all set names with a click of set icon.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+// jQuery.ajaxPrefilter(function(options) {
+//     if (options.crossDomain && jQuery.support.cors) {
+//         options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+//     }
+// });
+
+
 //////////////////    INDIVIDUAL CARDS    //////////////////////////////////
-$('form').on('submit' , (event) => {
+$('form').on('submit', (event) => {
     event.preventDefault();
     const card = $('input').val();
     $.ajax({
@@ -37,21 +44,77 @@ $('form').on('submit' , (event) => {
 
 
 ///////////////////////    Carousel    //////////////////////////////////
+//For CORS use jsonP
+
 $.ajax({
 //URL FOR SET NAMES AND ICONS
-url: `https://api.scryfall.com/sets/` 
+url: `https://api.scryfall.com/sets/`,
+dataType: "json",
+type: "GET",
+contentType: "application/json",
+crossDomain: true,
 }).then(
     (data) =>{
-        //console.log(data)
+        console.log(data)
         let highestIndex = data.data.length-1;
         let currentImageIndex = 0;
         for(let i = 0; i <= highestIndex; i++){
             $('<img class = SetSymbols>').attr({src: data.data[i].icon_svg_uri , class: `${i}`}).appendTo($('.carousel-images'));
-            $('.set-search').text(data.data[currentImageIndex].name);      
-        }          
+            $('.set-search').text(data.data[currentImageIndex].name);  
+        }
+
+
+
+        //Card Image Gallery
+
+
+
+    //Card Gallery Can Go Here
+
+        //Card Image Gallery
+    //    console.log(data.data[1].scryfall_uri)
+    //    const setArray = data.data[608].name
+    // const setArray = data.data[608].search_uri
+    
+    //console.log(`This is my setArray: ${setArray}`)
+
+    const setArray = data.data
+    // $('.carousel-images').on('click', () => {
+      
+    // })
+
+    $('.carousel-images').on('click', () => {
+        for(let i = 0; i <= highestIndex; i++){
+            $("<p></p>").text(data.data[i].name).appendTo($('#bottom'))
+        }
+    })
+
+    // for(let i = 0; i <= data.data.length-1; i++) {
+    //     $(`.carousel-images`).on('click', () => {
+    //         ($('<p>').attr('src' , data.data[i].name).appendTo($('#bottom')))
+    //     })
+    //  } 
+
+    //This gets me the names of each set
+    //const setArray = data.data
+    // setArray.forEach(data => {
+    //     console.log(data.name)
+    // })
+
+
+
+ //    setArray.forEach(data => console.log(data.scryfall_uri))
+
+ 
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////
+
+
         
         //Next Button
         $('.next').on('click' , () => {
+            console.log('next')
             $('.carousel-images').children().eq(currentImageIndex).css('display' , 'none');
 
             if(currentImageIndex < highestIndex) {
@@ -66,6 +129,7 @@ url: `https://api.scryfall.com/sets/`
         
         //Previous Buttons
         $('.previous').on('click' , () => {
+            console.log('previous')
             $('.carousel-images').children().eq(currentImageIndex).css('display', 'none');
         
             if(currentImageIndex > 0) {
@@ -78,47 +142,44 @@ url: `https://api.scryfall.com/sets/`
             $('.set-search').text(data.data[currentImageIndex].name);
         })
     }
+
+
 )
 
 
 
+     
+
+
+
+
+
+
+
+
 //CARD GALLERY
+// let setSearch = 
 // $.ajax({
-//     url: 'https://api.scryfall.com/cards/search?order=set&q=e%3Aaer&unique=prints'
+//     // url: 'https://api.scryfall.com/cards/search?order=set&q=e%3Aaer&unique=prints'
+//     url: `https://api.scryfall.com/cards/search?order=set&q=e%3A${setSearch}&unique=prints`,   
 // }).then(
 //     (data) => {
+        
 //         $('.carousel-images').on('click' , () => {
-//             for(let i = 0; i > data.data.length; i++) {
+//             console.log(data)
+//             //Notes 10/11/2020 Changed the for loop to i < data.data.length. It was i > data.data.length
+//             for(let i = 0; i < data.data.length; i++) {
+//                 $('.carousel-images').on('click' , () => {
+//                     console.log(data)
 //                 $('<img>').attr('src' , data.data[i].image_uris.normal).appendTo($('#bottom'));
+//             })
 //             } 
 //         })
 //     }
 // )
 
-
-// $('.carousel-images').on('click' , () => {
-//   $.ajax({
-//         //URL FOR CARD GALLERY
-//         url: 'https://api.scryfall.com/cards/search?order=set&q=e%3Aaer&unique=prints',   
-//   }).then(
-//       (data) => {
-//         for(let i = 0; i < data.data.length; i++) {
-//             $('<img>').attr('src' , data.data[i].image_uris.normal).appendTo($('#bottom'));
-//         }
-//       }
-//   )
-// })
+// $("#bottom").val();
+// $.ajax()
 
 
 
-
-
-
-
-/*                               THINGS TO ADD IN THE FUTURE
-REJECTION NOTICE IF CARD DOES NOT EXIST
-CARD IMAGE GALLERY
-DECK BUILDING FEATURE
-DRAFTING FEATURE
-LIMIT MY CAROUSEL TO CORE AND EXPANSION SETS INSTEAD OF EVERY PRINTING
-*/ 
