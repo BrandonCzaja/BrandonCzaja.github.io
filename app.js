11206ac4c9b6e102ca89da102dfc3b0791015441
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-01-07 11:59:04 -0600

    Removed console logs

--- app.js
+++ app.js
@@ -4,154 +4,137 @@
 //     }
 // });
 
-
 //////////////////    INDIVIDUAL CARDS    //////////////////////////////////
-$('form').on('submit', (event) => {
-    event.preventDefault();
-    const card = $('input').val();
-    $.ajax({
-        url:`https://api.magicthegathering.io/v1/cards?name=${card}`
-    }).then(
-        (data) => {
-            //console.log(data);
-                //Card Image
-                $('#card-image').html(`<img src = "${data.cards[0].imageUrl}">`);
-                //Mana Cost
-                $('#mana').html(data.cards[0].manaCost);
-                //Type
-                $('#type').html(data.cards[0].type);
-                //Rarity
-                $('#rarity').html(data.cards[0].rarity);
-                //Power & Toughness
-                if(data.cards[0].power === undefined) {
-                    $('#power').text('This card is not a creature');
-                } else {
-                $('#power').html(data.cards[0].power + '/' + data.cards[0].toughness);
-                }
-                //Card Text
-                if(data.cards[0].text === undefined) {
-                    $('#card-text').text('This card does not have any rules text');
-                } else {
-                $('#card-text').html(data.cards[0].text);
-                }
-            })    
-            }) , () => {
-                //THIS DOESN'T WORK
-            $('.split left').append($('<div>').text('ERROR: Please try another card name.'));
-            console.log('ERROR: Please try another card name.');
-        }
-
-
+$("form").on("submit", (event) => {
+  event.preventDefault();
+  const card = $("input").val();
+  $.ajax({
+    url: `https://api.magicthegathering.io/v1/cards?name=${card}`,
+  }).then((data) => {
+    //console.log(data);
+    //Card Image
+    $("#card-image").html(`<img src = "${data.cards[0].imageUrl}">`);
+    //Mana Cost
+    $("#mana").html(data.cards[0].manaCost);
+    //Type
+    $("#type").html(data.cards[0].type);
+    //Rarity
+    $("#rarity").html(data.cards[0].rarity);
+    //Power & Toughness
+    if (data.cards[0].power === undefined) {
+      $("#power").text("This card is not a creature");
+    } else {
+      $("#power").html(data.cards[0].power + "/" + data.cards[0].toughness);
+    }
+    //Card Text
+    if (data.cards[0].text === undefined) {
+      $("#card-text").text("This card does not have any rules text");
+    } else {
+      $("#card-text").html(data.cards[0].text);
+    }
+  });
+}),
+  () => {
+    //THIS DOESN'T WORK
+    // $('.split left').append($('<div>').text('ERROR: Please try another card name.'));
+    // console.log('ERROR: Please try another card name.');
+  };
 
 ///////////////////////    Carousel    //////////////////////////////////
 //For CORS use jsonP
 
 $.ajax({
-//URL FOR SET NAMES AND ICONS
-url: `https://api.scryfall.com/sets/`,
-dataType: "json",
-type: "GET",
-contentType: "application/json",
-crossDomain: true,
-}).then(
-    (data) =>{
-        console.log(data)
-        let highestIndex = data.data.length-1;
-        let currentImageIndex = 0;
-        //This makes the carousel
-        for(let i = 0; i <= highestIndex; i++){
-            $('<img class = SetSymbols>').attr({src: data.data[i].icon_svg_uri , class: `${i}`}).appendTo($('.carousel-images'));
-            $('.set-search').text(data.data[currentImageIndex].name);  
-        }
+  //URL FOR SET NAMES AND ICONS
+  url: `https://api.scryfall.com/sets/`,
+  dataType: "json",
+  type: "GET",
+  contentType: "application/json",
+  crossDomain: true,
+}).then((data) => {
+  let highestIndex = data.data.length - 1;
+  let currentImageIndex = 0;
+  //This makes the carousel
+  for (let i = 0; i <= highestIndex; i++) {
+    $("<img class = SetSymbols>")
+      .attr({ src: data.data[i].icon_svg_uri, class: `${i}` })
+      .appendTo($(".carousel-images"));
+    $(".set-search").text(data.data[currentImageIndex].name);
+  }
 
+  //Card Gallery
+  //Card Image Gallery
+  // $('.carousel-images').on('click', () => {
+  //     for(let j = 0; j <= highestIndex; j++){
+  //         //Can I change it to the icon class name?
+  //         $("<img>").attr({src: data.data[j].search_uri}).appendTo($('#bottom'))
+  //     }
+  // })
 
+  //const setArray = data.data
+  //Card Image Gallery
+  //    console.log(data.data[1].scryfall_uri)
+  //    const setArray = data.data[608].name
+  // const setArray = data.data[608].search_uri
 
-     
-        
+  //This will print out all the set names. I need to change .text() value to the icon value
+  // $('.carousel-images').on('click', () => {
+  //     for(let i = 0; i <= highestIndex; i++){
+  //         //Can I change it to the icon class name?
+  //         $("<p></p>").text(data.data[i].name).appendTo($('#bottom'))
+  //     }
+  // })
 
+  ////////////////////////////////////////////////////////////////////////////////////////////////
 
-    //Card Gallery 
-   //Card Image Gallery
-        // $('.carousel-images').on('click', () => {
-        //     for(let j = 0; j <= highestIndex; j++){
-        //         //Can I change it to the icon class name?
-        //         $("<img>").attr({src: data.data[j].search_uri}).appendTo($('#bottom'))
-        //     }
-        // })
+  //Next Button
+  $(".next").on("click", () => {
+    $(".carousel-images")
+      .children()
+      .eq(currentImageIndex)
+      .css("display", "none");
 
-    //const setArray = data.data
-        //Card Image Gallery
-    //    console.log(data.data[1].scryfall_uri)
-    //    const setArray = data.data[608].name
-    // const setArray = data.data[608].search_uri
-    
-
-    //This will print out all the set names. I need to change .text() value to the icon value
-    // $('.carousel-images').on('click', () => {
-    //     for(let i = 0; i <= highestIndex; i++){
-    //         //Can I change it to the icon class name?
-    //         $("<p></p>").text(data.data[i].name).appendTo($('#bottom'))
-    //     }
-    // })
-
-
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-        
-        //Next Button
-        $('.next').on('click' , () => {
-            console.log('next')
-            $('.carousel-images').children().eq(currentImageIndex).css('display' , 'none');
-
-            if(currentImageIndex < highestIndex) {
-                currentImageIndex++;
-            } else {
-                currentImageIndex = 0;
-            }
-        
-            $('.carousel-images').children().eq(currentImageIndex).css('display' , 'block');
-            $('.set-search').text(data.data[currentImageIndex].name);
-        })
-        
-        //Previous Buttons
-        $('.previous').on('click' , () => {
-            console.log('previous')
-            $('.carousel-images').children().eq(currentImageIndex).css('display', 'none');
-        
-            if(currentImageIndex > 0) {
-                currentImageIndex--;
-            } else {
-                currentImageIndex = highestIndex;
-            }
-        
-            $('.carousel-images').children().eq(currentImageIndex).css('display', 'block');
-            $('.set-search').text(data.data[currentImageIndex].name);
-        })
+    if (currentImageIndex < highestIndex) {
+      currentImageIndex++;
+    } else {
+      currentImageIndex = 0;
     }
 
+    $(".carousel-images")
+      .children()
+      .eq(currentImageIndex)
+      .css("display", "block");
+    $(".set-search").text(data.data[currentImageIndex].name);
+  });
 
-)
+  //Previous Buttons
+  $(".previous").on("click", () => {
+    $(".carousel-images")
+      .children()
+      .eq(currentImageIndex)
+      .css("display", "none");
 
+    if (currentImageIndex > 0) {
+      currentImageIndex--;
+    } else {
+      currentImageIndex = highestIndex;
+    }
 
-
-     
-
-
-
-
-
-
-
+    $(".carousel-images")
+      .children()
+      .eq(currentImageIndex)
+      .css("display", "block");
+    $(".set-search").text(data.data[currentImageIndex].name);
+  });
+});
 
 //CARD GALLERY
-// let setSearch = 
+// let setSearch =
 // $.ajax({
 //     // url: 'https://api.scryfall.com/cards/search?order=set&q=e%3Aaer&unique=prints'
-//     url: `https://api.scryfall.com/cards/search?order=set&q=e%3A${setSearch}&unique=prints`,   
+//     url: `https://api.scryfall.com/cards/search?order=set&q=e%3A${setSearch}&unique=prints`,
 // }).then(
 //     (data) => {
-        
+
 //         $('.carousel-images').on('click' , () => {
 //             console.log(data)
 //             //Notes 10/11/2020 Changed the for loop to i < data.data.length. It was i > data.data.length
@@ -160,13 +143,10 @@ crossDomain: true,
 //                     console.log(data)
 //                 $('<img>').attr('src' , data.data[i].image_uris.normal).appendTo($('#bottom'));
 //             })
-//             } 
+//             }
 //         })
 //     }
 // )
 
 // $("#bottom").val();
 // $.ajax()
-
-
-
